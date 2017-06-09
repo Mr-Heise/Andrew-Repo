12a20ccd74dcf6c6791935c75bf37eb83445172c
Andrew Metz <ametz1999@gmail.com>
2017-06-09 10:58:56 -0600

    Add files via upload

--- Divisions.java
+++ Divisions.java
@@ -15,13 +15,68 @@ import java.io.*;
 public class Divisions
 {
 
+	public static String determineDivision(int birthYear,int birthMonth)
+	{
+		
+		// Instantiate a variable of the string type to store the result
+		String result = "";
+		
+		// Declare four variables of the integer type to store the age, divison, and cut off age.
+		int SHSAAYear = 2016;
+		int age = 10;
+		int division = 0;
+		int cutOffAge;
+		
+		// Determine the cut off year
+		cutOffAge = SHSAAYear - birthYear;
+		
+		// Using an if else decision structure, determine the division of the student
+		if (birthMonth <= 8) {
+			do {
+
+				age++;
+				division = age;
+										
+			} while (age != cutOffAge);
+			
+		} else {
+			do {
+		
+				age++;
+				division = age - 1;
+										
+			} while (age != cutOffAge);
+			
+		}		
+			
+		switch (division) {
+		
+			case 10: result = "Wee Pee"; break;
+			case 11: result = "Wee Pee"; break;
+			case 12: result = "Pee Wee"; break;
+			case 13: result = "Bantam"; break;
+			case 14: result = "Midget"; break;
+			case 15: result = "Junior"; break;
+			case 16: result = "Senior"; break;
+			case 17: result = "Senior"; break;
+			
+		}
+		
+		return(result);
+	
+	}
+
 	public static void main(String[] args)
 	{
 		
-		// Instantiate three variables of the string type to store the line of text and the last and first name.		
+		// Instantiate four variables of the string type to store the line of text, the division of the student,
+		//		and the student's first and last name.		
 		String lineOfText;
-		String lastName;
+		String division;
 		String firstName;
+		String lastName;
+		
+		
 		
 		// Declare three variables of the integer type to store the birth year, birth month, and the position of the comma.
 		int birthYear;
@@ -44,7 +99,7 @@ public class Divisions
 			
 			while ((lineOfText = readFile.readLine()) != null) {
 				
-				// Begin parse string
+				// Parse the input string into the last name, first name, birth year, and birth month.
 				// Determine the last name of the student.
 				comma = lineOfText.indexOf(",");		
 				lastName = lineOfText.substring(0,comma);		
@@ -63,19 +118,12 @@ public class Divisions
 				// Determine the birth month of the student.
 				comma = lineOfText.indexOf(",");		
 				birthMonth = Integer.parseInt(lineOfText.substring(0,comma));		
-	
-				// Output the first name, last name, birth year, and birth month of the student.
-				System.out.println("\n\tFirst name: " + firstName);
-				System.out.println("\n\tLast name: " + lastName);
-				System.out.println("\n\tBirth Year: " + birthYear);
-				System.out.println("\n\tBirth Month: " + birthMonth);
-				// End parse string.
 				
-				// Begin determine track division.
+				// Determine track division.				
+				division = determineDivision(birthYear,birthMonth);
 				
-				
-				
-				// End determine track division.
+				System.out.println(firstName + " " + lastName + " is in the " + division + " division.\n");
+
 			}
 		
 		} catch (FileNotFoundException e) {
@@ -92,7 +140,7 @@ public class Divisions
 			
 		}
 		
-		System.out.println("\n");
+		System.out.println();
 			
 	}
 		
